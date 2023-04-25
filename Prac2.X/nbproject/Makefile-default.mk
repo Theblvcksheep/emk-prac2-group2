b09d11f81e452f0f6c1df52f27d99aecf2db34d6
Theblvcksheep <semousuneo@gmail.com>
2023-04-25 09:19:34 +0200

    added project file

--- Prac2.X/nbproject/Makefile-default.mk
+++ Prac2.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=
+SOURCEFILES_QUOTED_IF_SPACED=code_15_03_23_17_28.asm
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=
-POSSIBLE_DEPFILES=
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/code_15_03_23_17_28.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/code_15_03_23_17_28.o.d
 
 # Object Files
-OBJECTFILES=
+OBJECTFILES=${OBJECTDIR}/code_15_03_23_17_28.o
 
 # Source Files
-SOURCEFILES=
+SOURCEFILES=code_15_03_23_17_28.asm
 
 
 
@@ -95,7 +95,23 @@ MP_LINKER_DEBUG_OPTION=
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: assemble
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/code_15_03_23_17_28.o: code_15_03_23_17_28.asm  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/code_15_03_23_17_28.o.d 
+	@${RM} ${OBJECTDIR}/code_15_03_23_17_28.o 
+	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/code_15_03_23_17_28.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG  -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/code_15_03_23_17_28.lst\" -e\"${OBJECTDIR}/code_15_03_23_17_28.err\" $(ASM_OPTIONS)    -o\"${OBJECTDIR}/code_15_03_23_17_28.o\" \"code_15_03_23_17_28.asm\" 
+	@${DEP_GEN} -d "${OBJECTDIR}/code_15_03_23_17_28.o"
+	@${FIXDEPS} "${OBJECTDIR}/code_15_03_23_17_28.o.d" $(SILENT) -rsi ${MP_AS_DIR} -c18 
+	
 else
+${OBJECTDIR}/code_15_03_23_17_28.o: code_15_03_23_17_28.asm  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/code_15_03_23_17_28.o.d 
+	@${RM} ${OBJECTDIR}/code_15_03_23_17_28.o 
+	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/code_15_03_23_17_28.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/code_15_03_23_17_28.lst\" -e\"${OBJECTDIR}/code_15_03_23_17_28.err\" $(ASM_OPTIONS)    -o\"${OBJECTDIR}/code_15_03_23_17_28.o\" \"code_15_03_23_17_28.asm\" 
+	@${DEP_GEN} -d "${OBJECTDIR}/code_15_03_23_17_28.o"
+	@${FIXDEPS} "${OBJECTDIR}/code_15_03_23_17_28.o.d" $(SILENT) -rsi ${MP_AS_DIR} -c18 
+	
 endif
 
 # ------------------------------------------------------------------------------------
